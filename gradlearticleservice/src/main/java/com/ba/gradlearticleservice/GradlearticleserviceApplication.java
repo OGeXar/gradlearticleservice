60eb825efa43d06643e62f133486a6e1a4ca5c58
Jan <jan0000046@gmail.com>
2021-10-22 18:05:55 +0200

    init

---
+++ gradlearticleservice/src/main/java/com/ba/gradlearticleservice/GradlearticleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ba.gradlearticleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlearticleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlearticleserviceApplication.class, args);
+	}
+
+}
